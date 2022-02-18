4b7585dea321ae4cb0f77d1c1100d9bc3b9bd6cd
Draymonders <Draymonders@gmail.com>
2022-02-18 14:56:06 +0800

    定位问题 反序列化 数字不一致问题

--- json/json.go
+++ json/json.go
@@ -5,10 +5,22 @@ import (
 	jsoniter "github.com/json-iterator/go"
 )
 
-var json = jsoniter.ConfigCompatibleWithStandardLibrary
+var (
+	api  jsoniter.API
+)
+
+func init() {
+	json := jsoniter.Config{
+		EscapeHTML:             true,
+		SortMapKeys:            true,
+		ValidateJsonRawMessage: true,
+		UseNumber:              true,
+	}
+	api = json.Froze()
+}
 
 func ObjToJson(obj interface{}) string {
-	s, err := json.MarshalToString(obj)
+	s, err := api.MarshalToString(obj)
 	if err != nil {
 		log.Error("marshal err. %+v", err)
 	}
@@ -16,5 +28,5 @@ func ObjToJson(obj interface{}) string {
 }
 
 func JsonToObj(str string, obj interface{}) error {
-	return json.UnmarshalFromString(str, obj)
+	return api.UnmarshalFromString(str, obj)
 }
--- json/json_test.go
+++ json/json_test.go
@@ -7,7 +7,7 @@ import (
 
 func Test_Id_Unmarshal(t *testing.T) {
 	str := "{\"poi_id_v2\":6980984069106436127}"
-	var data map[string]interface{}
+	data := map[string]interface{}{}
 	err := JsonToObj(str, &data)
 	if err != nil {
 		fmt.Println("json err")
@@ -15,8 +15,17 @@ func Test_Id_Unmarshal(t *testing.T) {
 	}
 	fmt.Println(data)
 	for _, v := range data {
-		fmt.Printf("%f", v)
+		fmt.Printf("%v", v)
 	}
 	//	map[poi_id_v2:6.980984069106436e+18]
 	//	6980984069106436096.000000
+}
+
+func Test_Uint64_To_Float64(t *testing.T) {
+	// 这种情况下会出现不一致
+	v := float64(uint64(6980984069106436127))
+	fmt.Println(v)
+	fmt.Printf("%f\n", v)
+	// 6.980984069106436e+18
+	// 6980984069106436096.000000
 }
